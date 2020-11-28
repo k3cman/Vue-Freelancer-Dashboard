b89b00491a8e126dc5731693eb7ac01db93b821a
Kecman <kecman.dev@gmail.com>
2020-11-28 23:05:08 +0100

    add pages

---
+++ src/components/Shared/TrackItems/TrackItemsHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="track-item-header">
+    {{ timestamp }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    timestamp: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.track-item-header {
+  margin-top: 30px;
+  background: black;
+  color: white;
+  padding: 0 10px;
+  font-size: 12px;
+  display: flex;
+  justify-content: flex-end;
+}
+</style>
--- src/views/Tracker.vue
+++ src/views/Tracker.vue
@@ -1,3 +1,93 @@
 <template>
-  TRACKER
+  <track-items-wrapper :datasets="datasets"></track-items-wrapper>
 </template>
+
+<script>
+import moment from "moment";
+import TrackItemsWrapper from "../components/Shared/TrackItems/TrackItemsWrapper.vue";
+export default {
+  components: {
+    TrackItemsWrapper,
+  },
+  data() {
+    return {
+      datasets: [
+        {
+          timestamp: moment()
+            .subtract(1, "day")
+            .format("DD/MM/YYYY"),
+          events: [
+            {
+              id: 1,
+              name: "Simple task 1",
+              startTime: moment()
+                .subtract(1, "day")
+                .subtract(3, "hours")
+                .toDate(),
+              endTime: moment()
+                .subtract(1, "day")
+                .subtract(1, "hour")
+                .toDate(),
+              time: 2,
+              project: "estateOs",
+              billable: true,
+            },
+            {
+              id: 2,
+              name: "Simple task 2",
+              startTime: moment()
+                .subtract(1, "day")
+                .subtract(6, "hours")
+                .toDate(),
+              endTime: moment()
+                .subtract(1, "day")
+                .subtract(4, "hour")
+                .toDate(),
+              time: 2,
+              project: "estateOs",
+              billable: true,
+            },
+          ],
+        },
+        {
+          timestamp: moment()
+            .subtract(2, "days")
+            .format("DD/MM/YYYY"),
+          events: [
+            {
+              id: 1,
+              name: "Simple task 1",
+              startTime: moment()
+                .subtract(2, "days")
+                .subtract(3, "hours")
+                .toDate(),
+              endTime: moment()
+                .subtract(2, "days")
+                .subtract(1, "hour")
+                .toDate(),
+              time: 2,
+              project: "estateOs",
+              billable: true,
+            },
+            {
+              id: 2,
+              name: "Simple task 2",
+              startTime: moment()
+                .subtract(2, "days")
+                .subtract(6, "hours")
+                .toDate(),
+              endTime: moment()
+                .subtract(2, "days")
+                .subtract(4, "hour")
+                .toDate(),
+              time: 2,
+              project: "estateOs",
+              billable: true,
+            },
+          ],
+        },
+      ],
+    };
+  },
+};
+</script>
