bb0e036d147d4109c7552564e366b5be85461590
k3cman <kecman.dev@gmail.com>
2020-11-28 23:26:54 +0100

    refactor names

--- src/views/Tracker/Tracker.vue
+++ src/views/Tracker/Tracker.vue
@@ -1,13 +1,32 @@
 <template>
-  <track-items-wrapper :datasets="datasets"></track-items-wrapper>
+  <div class="new">
+    <new-entry></new-entry>
+  </div>
+  <div class="track-items-wrapper">
+    <div class="history" v-for="(dataset, index) in datasets" :key="index">
+      <entry-group-header :timestamp="dataset.timestamp"></entry-group-header>
+      <div class="table">
+        <entry-row
+            v-for="event in dataset.events"
+            :key="event.id"
+            :data="event"
+        ></entry-row>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import moment from "moment";
-import TrackItemsWrapper from "../components/Shared/TrackItems/TrackItemsWrapper.vue";
+import EntryRow from "@/components/Shared/Entries/EntryRow";
+import EntryGroupHeader from "@/components/Shared/Entries/EntryGroupHeader";
+import NewEntry from '@/components/Shared/Entries/NewEntry'
+
 export default {
   components: {
-    TrackItemsWrapper,
+    EntryGroupHeader,
+    EntryRow,
+    NewEntry
   },
   data() {
     return {
